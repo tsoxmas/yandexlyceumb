e49e4380bfc0b08c0ff15044838fc20e30914d72
tsoxmas <catslover0@gmail.com>
2020-05-11 19:45:00 +0500

    g
    gf

--- bot/id.py
+++
@@ -1,9 +0,0 @@
-import json
-from database import DataBase
-
-
-class IdGetter:
-    def handle_msf(self, msg):
-        if msg['text'] == '/start':
-            id = msg['id']
-            return id
--- bot/jsonresponse.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class JSONResponse:
-    code: int or None
-    content: str or None
-    error: Exception or None
-    additional: str or None = None
